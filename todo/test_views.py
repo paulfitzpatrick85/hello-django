ed3401ba8a1a6d100a240fbd78f07ca9e5df8f97
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-06 12:59:02 +0000

    django testing vid

--- todo/test_views.py
+++ todo/test_views.py
@@ -6,11 +6,3 @@ class TestDjango(TestCase):
     def test_this_thing_works(self):  #self refers to test django class 
         self.assertEqual(1, 1)  # compare/check if 1 == 0
 
-    def test_this_thing_works1(self):  #self refers to test django class 
-        self.assertEqual(1, 2)  # compare/check if 1 == 0
-
-
-    def test_this_thing_works2(self):  #self refers to test django class 
-        self.assertEqual(1, )  # compare/check if 1 == 0
-
-
