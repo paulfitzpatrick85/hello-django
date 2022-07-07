08b5e440d6a5dfa4a8e26d0905b9a72bd17d67ca
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-07 17:42:44 +0000

    coverage

--- todo/test_views.py
+++ todo/test_views.py
@@ -42,3 +42,10 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)  #get item again
         self.assertFalse(updated_item.done)  # check done status
+
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
