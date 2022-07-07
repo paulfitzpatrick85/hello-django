17e916d3c49d81b85bf4f9f02b2387476d5f3009
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-07 22:14:39 +0000

    allowed host error

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SCRET_KEY', 'django-insecure-@c_-b)u^)=hk(2sh$m_0(fave#z%i6s&4vgb#+cn4%q+l&l7v8')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@c_-b)u^)=hk(2sh$m_0(fave#z%i6s&4vgb#+cn4%q+l&l7v8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['pf-django-todo-app.herokuapp.com']
 
 
 # Application definition
